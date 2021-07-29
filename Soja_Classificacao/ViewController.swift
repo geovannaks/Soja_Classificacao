eb685de988f8fc7217515f1e9eb9c11a464d6d56
geovannaks <geovanna.kasemirinski@gmail.com>
2021-07-28 22:31:40 -0300

    Pergunta 1 e fontes novas

--- Soja_Classificacao/ViewController.swift
+++ Soja_Classificacao/ViewController.swift
@@ -7,8 +7,43 @@
 
 import UIKit
 
+@IBDesignable extension UIButton {
+
+    @IBInspectable var borderWidth: CGFloat {
+        set {
+            layer.borderWidth = newValue
+        }
+        get {
+            return layer.borderWidth
+        }
+    }
+
+    @IBInspectable var cornerRadius: CGFloat {
+        set {
+            layer.cornerRadius = newValue
+        }
+        get {
+            return layer.cornerRadius
+        }
+    }
+
+    @IBInspectable var borderColor: UIColor? {
+        set {
+            guard let uiColor = newValue else { return }
+            layer.borderColor = uiColor.cgColor
+        }
+        get {
+            guard let color = layer.borderColor else { return nil }
+            return UIColor(cgColor: color)
+        }
+    }
+}
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var pergunta1Label: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
